5991e12b2e625438f2d3a67917725e2fdf235557
Ankit Kumar <ankitkr00.india@gmail.com>
2023-07-25 22:50:04 +0530

    merging

--- src/main/java/com/modifyz/PanelApplicaton/facilityDetails/controller/FacilityController.java
+++ src/main/java/com/modifyz/PanelApplicaton/facilityDetails/controller/FacilityController.java
@@ -1,12 +1,18 @@
 package com.modifyz.PanelApplicaton.facilityDetails.controller;
 
-import com.modifyz.PanelApplicaton.facilityDetails.domain.Facility;
 import com.modifyz.PanelApplicaton.facilityDetails.dto.FacilityDto;
 import com.modifyz.PanelApplicaton.facilityDetails.service.FacilityService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +21,30 @@ public class FacilityController {
 
     @Autowired FacilityService facilityService;
 
-    @RequestMapping("/")
-    public ResponseEntity<Facility> createOrUpdateService(FacilityDto facilityDto) {
-        Facility facility = facilityService.createOrUpdate(facilityDto);
+    @PostMapping("/")
+    public ResponseEntity<FacilityDto> createFacility(@RequestBody FacilityDto facilityDto) {
+        FacilityDto facility = facilityService.createFacility(facilityDto);
         return new ResponseEntity<>(facility, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{shopId}")
+    public ResponseEntity<FacilityDto> updateFacility(@PathVariable String shopId,
+        @RequestBody FacilityDto facilityDto) {
+        FacilityDto facility = facilityService.updateFacility(shopId, facilityDto);
+        return new ResponseEntity<>(facility, HttpStatus.OK);
+    }
+
+    @GetMapping("/shop/{shopId}/getFacility/")
+    public ResponseEntity<List<FacilityDto>> getFacilityByShopId(@PathVariable String shopId) {
+        List<FacilityDto> facilitiesList = facilityService.getFacilityByShopId(shopId);
+        return new ResponseEntity<>(facilitiesList, HttpStatus.OK);
+    }
+
+    // TODO -- Dev Test is pending
+    @GetMapping("/{shopId}/getFacility/{serviceName}")
+    public ResponseEntity<FacilityDto> getFacilityByServiceName(@PathVariable String shopId,
+        @RequestParam String serviceName) {
+        FacilityDto facility = facilityService.getFacilityByShopIdAndServiceName(shopId, serviceName);
+        return new ResponseEntity<>(facility, HttpStatus.OK);
     }
 }
